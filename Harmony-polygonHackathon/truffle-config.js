8860d706da4068e20aa188709f45dd654e7ff916
BrianMburu <brian.mburu@students.jkuat.ac.ke>
2022-04-10 12:18:05 +0300

    second commit

---
+++ Harmony-polygonHackathon/truffle-config.js
@@ -0,0 +1,31 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const privateKeyTest = '776e884d682f64f4d1835b260b42a6a27669355d7b478e30d94e9bb6414a464e';
+
+module.exports = {
+  networks: {
+    testnet: {
+      provider: () => {
+        return new HDWalletProvider({
+          mnemonic,
+          providerOrUrl: 'https://api.s0.b.hmny.io', // https://api.s0.t.hmny.io for mainnet
+          derivationPath: `m/44'/1023'/0'/0/`
+        });
+      },
+      network_id: 1666700000, // 1666600000 for mainnet
+    },
+    testnetHarmony: {
+      provider: () => {
+        if (!privateKeyTest.trim()) {
+          throw new Error(
+            'Please enter a private key with funds, you can use the default one'
+          );
+        }
+        return new HDWalletProvider({
+          privateKeys: [privateKeyTest],
+          providerOrUrl: 'https://api.s0.b.hmny.io',
+        });
+      },
+      network_id: 1666700000,
+    },
+  },
+};
